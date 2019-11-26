80c82cdcebedd829c00d6cc513bf5336ff18f361
Ricardo Pontes <rpontes@gmail.com>
2019-11-25 23:10:29 -0300

    Create file to execute the example values

---
+++ execute.rb
@@ -0,0 +1,19 @@
+require_relative './mars'
+require_relative './probe'
+require_relative './explore'
+
+mars = Mars.new(5, 5)
+
+probe = Probe.new(1, 2, 'N')
+explore = Explore.new(mars, probe, 'LMLMLMLMM')
+explore.move
+
+p 'Space Probe: 1 2 N / LMLMLMLMM'
+p "Final destination: #{explore.final_coordinate}"
+
+probe = Probe.new(3, 3, 'E')
+explore = Explore.new(mars, probe, 'MMRMMRMRRM')
+explore.move
+
+p 'Space Probe: 3 3 E / MMRMMRMRRM'
+p "Final destination: #{explore.final_coordinate}"
